14a71497cb50500c151e79d72b506b14eb1f4632
Brijesh Harry <89608615+Bsharma4@users.noreply.github.com>
2022-02-16 12:49:11 -0600

    Update Main_Class.py
    Created a super class for controllers

--- Main_Class.py
+++ Main_Class.py
@@ -1,4 +1,7 @@
-class SimpleController:
+import board
+import neopixel
+
+class Controller:
     DOWN = 144
 
     def __init__(self, num_lights, color_on, color_off):
@@ -15,15 +18,3 @@ class SimpleController:
                                         brightness=1,
                                         pixel_order=neopixel.RGB)
         self.pixels.fill((0,0,0))
-
-
-    def process_event(self, event):
-        message, deltatime = event
-        state = message[0]
-        print(message, deltatime)
-        if state == SimpleController.DOWN:
-            self.pixels[self.next_light % self.num_lights] = self.color_on
-            self.next_light+=1
-        else:
-            self.pixels[self.prev_light % self.num_lights] = self.color_off
-            self.prev_light+=1
